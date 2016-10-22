5d400d4fa86f7d5df14f1fef5b00380f9e9d6ce0
bertojanza <bertojanza@yahoo.es>
2016-10-22 10:11:38 -0400

    First three assigments and gitignore
      First three assigments and gitignore

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -3,13 +3,13 @@ import pandas as pd
 # TODO: Load up the 'tutorial.csv' dataset
 #
 # .. your code here ..
-
-
+df = pd.read_csv('/home/alberto/workspace/DAT210x/Module2/Datasets/tutorial.csv')
+print(df)
 
 # TODO: Print the results of the .describe() method
 #
 # .. your code here ..
-
+df.describe()
 
 
 # TODO: Figure out which indexing method you need to
@@ -17,4 +17,7 @@ import pandas as pd
 # And print the results
 #
 # .. your code here ..
-
+#df.col3[2:4]
+#df.iloc[2:4, 3]
+#df.ix[2:4, 'col3']
+df.loc[2:4, 'col3']
--- Module2/assignment3.py
+++ Module2/assignment3.py
@@ -4,14 +4,18 @@ import pandas as pd
 # Ensuring you set the appropriate header column names
 #
 # .. your code here ..
+df = pd.read_csv('/home/alberto/workspace/DAT210x/Module2/Datasets/servoo.data', names=['motor', 'screw', 'pgain', 'vgain', 'class'])
 
+df = pd.read_csv('DAT210x/Module2/Datasets/servoo.data', names=['motor', 'screw', 'pgain', 'vgain', 'class'])
 
 # TODO: Create a slice that contains all entries
 # having a vgain equal to 5. Then print the 
 # length of (# of samples in) that slice:
 #
 # .. your code here ..
-
+vagain_5 = df[(df.vgain == 5)]
+len(vagain_5)
+# 22
 
 # TODO: Create a slice that contains all entries
 # having a motor equal to E and screw equal
@@ -19,8 +23,8 @@ import pandas as pd
 # samples in) that slice:
 #
 # .. your code here ..
-
-
+len(df[(df.motor == 'E') & (df.screw == 'E')])
+# 6
 
 # TODO: Create a slice that contains all entries
 # having a pgain equal to 4. Use one of the
@@ -29,11 +33,19 @@ import pandas as pd
 # you've found it, print it:
 #
 # .. your code here ..
-
+pgain_4 = df[(df.pgain == 4)]
+pgain_4.vgain.mean()
+# 2.0606060606060606
 
 
 # TODO: (Bonus) See what happens when you run
 # the .dtypes method on your dataframe!
+pgain_4.dtypes
 
-
+#motor     object
+#screw     object
+#pgain      int64
+#vgain      int64
+#class    float64
+#dtype: object
 
